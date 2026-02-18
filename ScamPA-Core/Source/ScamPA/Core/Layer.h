668923a86e3a1ef6514891d7ad9b1057f4b05c28
cpap7 <mjajarmi2000@outlook.com>
2026-02-17 23:34:16 -0500

    Updated Layer API w/ some documentation detailing the purpose of different functions

--- ScamPA-Core/Source/ScamPA/Core/Layer.h
+++ ScamPA-Core/Source/ScamPA/Core/Layer.h
@@ -7,13 +7,12 @@ namespace SPA {
 	public:
 		virtual ~ILayer() = default;
 
-		// Defined clientside, as well as in derived classes (i.e., CImGuiLayer)
-		virtual void OnAttach() {}
-		virtual void OnDetach() {}
-		virtual void OnUpdate(float a_timestep) {}
-		virtual void OnUIRender() {}
-		virtual void OnEvent(IEvent& a_event) {}
-
+		// Methods are defined in derived classes (i.e., CImGuiLayer) or layer classes within clientside app
+		virtual void OnAttach() {}									// Function for what happens when pushed onto a layer stack
+		virtual void OnDetach() {}									// Function for what happens when popped off of a layer stack
+		virtual void OnUpdate(float a_timestep) {}					// Function for what occurs w/ the layer every frame
+		virtual void OnUIRender() {}								// Function for whats occurs when rendering ImGui windows
+		virtual void OnEvent(IEvent& a_event) {}					// Function for handling events
 	};
 
 }

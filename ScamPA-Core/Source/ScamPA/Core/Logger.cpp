9eb1332dff56377eba0ad1c81d0336489f788f95
cpap7 <mjajarmi2000@outlook.com>
2026-04-08 16:17:50 -0400

    Made improvements to the logging system

--- ScamPA-Core/Source/ScamPA/Core/Logger.cpp
+++ ScamPA-Core/Source/ScamPA/Core/Logger.cpp
@@ -1,19 +1,37 @@
 #include "spapch.h"
 #include "Logger.h"
-
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace SPA {
 	std::shared_ptr<spdlog::logger> CLogger::s_core_logger;
 	std::shared_ptr<spdlog::logger> CLogger::s_client_logger;
 
+	std::shared_ptr<CImGuiConsoleSink> CLogger::s_core_console_sink;
+	std::shared_ptr<CImGuiConsoleSink> CLogger::s_client_console_sink;
+
 	void CLogger::Init() {
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+		// Create stdout color sinks
+		auto core_stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		auto client_stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
-		s_core_logger = spdlog::stdout_color_mt("[CORE]");
-		s_client_logger = spdlog::stdout_color_mt("[CLIENT]");
+		// Create ImGui console sinks
+		s_core_console_sink = std::make_shared<CImGuiConsoleSink>();
+		s_client_console_sink = std::make_shared<CImGuiConsoleSink>();
 
+		// Core logger (stdout + console panel)
+		std::vector<spdlog::sink_ptr> core_sinks = { core_stdout_sink, s_core_console_sink };
+		s_core_logger = std::make_shared<spdlog::logger>("[CORE]", core_sinks.begin(), core_sinks.end());
 		s_core_logger->set_level(spdlog::level::trace);
+		s_core_logger->set_pattern("%^[%T] %n: %v%$");
+
+		// Client logger (stdout + console panel)
+		std::vector<spdlog::sink_ptr> client_sinks = { client_stdout_sink, s_client_console_sink };
+		s_client_logger = std::make_shared<spdlog::logger>("[CLIENT]", client_sinks.begin(), client_sinks.end());
 		s_client_logger->set_level(spdlog::level::trace);
+		s_client_logger->set_pattern("%^[%T] %n: %v%$");
+
+		// Register with spdlog 
+		spdlog::register_logger(s_core_logger);
+		spdlog::register_logger(s_client_logger);
 	}
 }
--- ScamPA-Core/Source/ScamPA/Core/Logger.h
+++ ScamPA-Core/Source/ScamPA/Core/Logger.h
@@ -1,23 +1,88 @@
 #pragma once
 #include "Base.h"
+#include "LoggerCustomFormatters.h"
+#include "../ImGui/ImGuiConsoleSink.h"
 
 #include <spdlog/spdlog.h>
-#include <spdlog/fmt/bundled/ostream.h>
+#include <spdlog/fmt/ostr.h>
 
-#include <glm/glm.hpp>
+#include <map>
+
+#define SPA_ASSERT_MESSAGE_BOX (!SPA_DIST && SPA_PLATFORM_WINDOWS)
+
+#ifdef SPA_ASSERT_MESSAGE_BOX
+	#ifdef SPA_PLATFORM_WINDOWS
+		#include <Windows.h>
+	#endif
+#endif
 
 namespace SPA {
 	class CLogger {
+	public:
+		enum class EType : uint8_t {
+			Core	= 0,
+			Client	= 1,
+		};
+		
+		enum class ELevel : uint8_t {
+			Trace = 0,
+			Info,
+			Warn,
+			Error,
+			Fatal
+		};
+		
+		struct STagDetails {
+			ELevel m_level_filter = ELevel::Trace;
+			bool m_enabled = true;
+		};
+
 	private:
 		static std::shared_ptr<spdlog::logger> s_core_logger;
 		static std::shared_ptr<spdlog::logger> s_client_logger;
+		static std::shared_ptr<CImGuiConsoleSink> s_client_console_sink;
+		static std::shared_ptr<CImGuiConsoleSink> s_core_console_sink;
+
+		static inline std::map<std::string, STagDetails> s_enabled_tags;
 
 	public:
 		static void Init();
 
-		static inline std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_core_logger; }
-		static inline std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_client_logger; }
+		static inline std::shared_ptr<spdlog::logger>& GetCoreLogger()		{ return s_core_logger; }
+		static inline std::shared_ptr<spdlog::logger>& GetClientLogger()	{ return s_client_logger; }
+		static inline std::shared_ptr<CImGuiConsoleSink>& GetCoreConsoleSink() { return s_core_console_sink; }
+		static inline std::shared_ptr<CImGuiConsoleSink>& GetClientConsoleSink() { return s_client_console_sink; }
 
+		static inline bool HasTag(const std::string& tag) { return s_enabled_tags.find(tag) != s_enabled_tags.end(); }
+		static inline std::map<std::string, STagDetails>& EnabledTags() { return s_enabled_tags; }
+
+		template<typename... Args>
+		static void PrintMessage(CLogger::EType a_type, CLogger::ELevel a_level, std::string_view a_tag, std::string_view a_fmt, Args&&... a_args);
+
+		template<typename... Args>
+		static void PrintAssertMessage(CLogger::EType a_type, std::string_view a_prefix, Args&&... a_args);
+
+	public: // Enum Utils
+		static const char* LevelToString(ELevel a_level) {
+			switch (a_level) {
+				case ELevel::Trace: return "Trace";
+				case ELevel::Info:	return "Info";
+				case ELevel::Warn:	return "Warn";
+				case ELevel::Error:	return "Error";
+				case ELevel::Fatal:	return "Fatal";
+			}
+			return "Trace"; // Default
+		}
+
+		static ELevel StringToLevel(std::string_view a_level_string) {
+			if (a_level_string == "Trace")	{ return ELevel::Trace; }
+			if (a_level_string == "Info")	{ return ELevel::Info;	}
+			if (a_level_string == "Warn")	{ return ELevel::Warn;	}
+			if (a_level_string == "Error")	{ return ELevel::Error; }
+			if (a_level_string == "Fatal")	{ return ELevel::Fatal; }
+
+			return ELevel::Trace;
+		}
 
 	};
 }
@@ -37,6 +102,42 @@ OStream& operator <<(OStream& a_ostream, const glm::vec4& a_vector) {
 	return a_ostream << '(' << a_vector.x << ", " << a_vector.y << ", " << a_vector.z << ", " << a_vector.w << ")";
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Tagged logs (prefer these!)                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Core logging
+#define SPA_CORE_TRACE_TAG(tag, ...) ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Trace, tag, __VA_ARGS__)
+#define SPA_CORE_INFO_TAG(tag, ...)  ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Info, tag, __VA_ARGS__)
+#define SPA_CORE_WARN_TAG(tag, ...)  ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Warn, tag, __VA_ARGS__)
+#define SPA_CORE_ERROR_TAG(tag, ...) ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Error, tag, __VA_ARGS__)
+#define SPA_CORE_FATAL_TAG(tag, ...) ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Fatal, tag, __VA_ARGS__)
+
+// Client logging
+#define SPA_CLIENT_TRACE_TAG(tag, ...) ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Trace, tag, __VA_ARGS__)
+#define SPA_CLIENT_INFO_TAG(tag, ...)  ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Info, tag, __VA_ARGS__)
+#define SPA_CLIENT_WARN_TAG(tag, ...)  ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Warn, tag, __VA_ARGS__)
+#define SPA_CLIENT_ERROR_TAG(tag, ...) ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Error, tag, __VA_ARGS__)
+#define SPA_CLIENT_FATAL_TAG(tag, ...) ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Fatal, tag, __VA_ARGS__)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Core Logging
+#define SPA_CORE_TRACE(...)  ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Trace, "", __VA_ARGS__)
+#define SPA_CORE_INFO(...)   ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Info, "", __VA_ARGS__)
+#define SPA_CORE_WARN(...)   ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Warn, "", __VA_ARGS__)
+#define SPA_CORE_ERROR(...)  ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Error, "", __VA_ARGS__)
+#define SPA_CORE_FATAL(...)  ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Core, ::SPA::CLogger::ELevel::Fatal, "", __VA_ARGS__)
+
+// Client Logging
+#define SPA_CLIENT_TRACE(...)   ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Trace, "", __VA_ARGS__)
+#define SPA_CLIENT_INFO(...)    ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Info, "", __VA_ARGS__)
+#define SPA_CLIENT_WARN(...)    ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Warn, "", __VA_ARGS__)
+#define SPA_CLIENT_ERROR(...)   ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Error, "", __VA_ARGS__)
+#define SPA_CLIENT_FATAL(...)   ::SPA::CLogger::PrintMessage(::SPA::CLogger::EType::Client, ::SPA::CLogger::ELevel::Fatal, "", __VA_ARGS__)
+
+
+/*
 // Core logging macros
 #define SPA_CORE_TRACE(...)			SPA::CLogger::GetCoreLogger()->trace(__VA_ARGS__)
 #define SPA_CORE_INFO(...)			SPA::CLogger::GetCoreLogger()->info(__VA_ARGS__)
@@ -50,3 +151,46 @@ OStream& operator <<(OStream& a_ostream, const glm::vec4& a_vector) {
 #define SPA_CLIENT_WARN(...)		SPA::CLogger::GetClientLogger()->warn(__VA_ARGS__)
 #define SPA_CLIENT_ERROR(...)		SPA::CLogger::GetClientLogger()->error(__VA_ARGS__)
 #define SPA_CLIENT_CRITICAL(...)	SPA::CLogger::GetClientLogger()->critical(__VA_ARGS__)
+*/
+
+namespace SPA {
+	template<typename... Args>
+	void CLogger::PrintMessage(CLogger::EType a_type, CLogger::ELevel a_level, std::string_view a_tag, std::string_view a_fmt, Args&&... a_args) {
+		auto detail = s_enabled_tags[std::string(a_tag)];
+		
+		if (detail.m_enabled && detail.m_level_filter <= a_level) {
+			auto logger = (a_type == EType::Core) ? GetCoreLogger() : GetClientLogger();
+			std::string log_string = a_tag.empty() ? "{0}{1}" : "[{0}] {1}";
+			std::string formatted_string = fmt::format(fmt::runtime(log_string), a_tag, fmt::vformat(a_fmt, fmt::make_format_args(a_args...)));
+
+			switch (a_level) {
+				case ELevel::Trace: logger->trace("{}", formatted_string);		break;
+				case ELevel::Info:	logger->info("{}", formatted_string);		break;
+				case ELevel::Warn:	logger->warn("{}", formatted_string);		break;
+				case ELevel::Error: logger->error("{}", formatted_string);		break;
+				case ELevel::Fatal: logger->critical("{}", formatted_string);	break;
+			}
+		}
+	}
+
+	template<typename... Args>
+	void CLogger::PrintAssertMessage(CLogger::EType a_type, std::string_view a_prefix, Args&&... a_args) {
+		auto logger = (a_type == EType::Core) ? GetCoreLogger() : GetClientLogger();
+		logger->error("{0}: {1}", a_prefix, fmt::vformat(fmt::make_format_args(a_args...)));
+
+#ifdef SPA_ASSERT_MESSAGE_BOX
+		std::string message = fmt::vformat(fmt::make_format_args(a_args...));
+		MessageBoxA(nullptr, message.c_str(), "ScamPA Assert", MB_OK | MB_ICONERROR);
+#endif
+	}
+
+	template<>
+	inline void CLogger::PrintAssertMessage(CLogger::EType a_type, std::string_view a_prefix) {
+		auto logger = (a_type == EType::Core) ? GetCoreLogger() : GetClientLogger();
+		logger->error("{0}", a_prefix);
+
+#ifdef SPA_ASSERT_MESSAGE_BOX
+		MessageBoxA(nullptr, "No Message", "ScamPA Assert", MB_OK | MB_ICONERROR);
+#endif
+	}
+}
